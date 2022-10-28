6a60a3b6354a7bd905352e72e0d9110f1c8cfb4d
张宇 <yu.zhang@k7.cn>
2022-10-28 13:52:50 +0800

    feat: 补充部分工具类及其方法声明

--- lib/cuon.d.ts
+++ lib/cuon.d.ts
@@ -2,4 +2,22 @@ declare module cuon {
     export var getWebGLContext: (canvas: HTMLElement, opt_debug?: boolean)=>WebGLRenderingContext;
     export var initShaders: (gl: WebGLRenderbuffer, vshader: string, fshader: string)=>boolean;
     export var getProgram: (gl: any)=>WebGLProgram;
+    export class Vector3 {
+
+    }
+    export class Vector4 {
+
+    }
+    export class Matrix4 {
+        constructor(opt_src?: Matrix4);
+        setIdentity(): Matrix4;
+        set(src: Matrix4): Matrix4;
+        concat(other: Matrix4): Matrix4;
+        multiply(other: Matrix4): Matrix4;
+        multiplyVector3(pos: Vector3): Vector3;
+        multiplyVector4(pos: Vector4): Vector4;
+        transpose(): Matrix4;
+        setInverseOf(other: Matrix4): Matrix4;
+        invert(): Matrix4;
+    }
 }
